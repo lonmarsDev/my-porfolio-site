63b6d3da098f55acbd70dd0c43a45e95fa791635
lonmars <marlonpamisa@gmail.com>
2020-12-01 18:08:30 +0800

    update profolio link

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -180,7 +180,7 @@ export default class Projects extends React.Component {
 							</div>
 							<div className="modal-bottom">
 								<a
-									href="http://turbo-live.hozzbydigitalsolutions.com/"
+									href="https://turbo-bpo.com/"
 									target="_blank">
 									<h3>View live site</h3>
 								</a>
@@ -282,6 +282,82 @@ export default class Projects extends React.Component {
 							</div>
 						</div>
 					</div>
+
+					<div id="todolist" className="modal-card">
+						<div className="visual">
+							<img src={todo} alt="" />
+						</div>
+						<div className="modal-info">
+							<h2>Dapp Store</h2>
+							<div className="modal-description">
+								<ul>
+									<li>
+										Integration with  KYC as an identity management system for identifying and screening shareholders both individuals and corporate entities.
+									</li>
+									<li>
+										Immutable and irreversible records of all transactions which are timestamped and auditable.
+									</li>
+									<li>
+										Shareholders can view and manage their portfolio of securities in near real-time.
+									</li>
+									{/* <li>
+										Support for OTC trades between shareholders requiring private key signatures from both participants and admin.
+									</li>
+									<li>
+										Support for share issuance, share cancellation and share splits requiring private key signature from issuer.
+									</li>
+									<li>
+										Allow issuers to distribute shares and currencies for IPO and dividends.
+									</li>
+									<li>
+										Allow complete viewing of shareholding movements which will provide regulators with near real-time visibility of substantial movements.
+									</li>
+									<li>
+										Ready for integration to support withdrawals and deposits by shareholders.
+									</li>
+									<li>
+										Support for multiple currencies represented as transferable asset on the blockchain.
+									</li> */}
+									<li>
+										Support for asset transfers from one shareholder to another.
+									</li>
+									<li>
+										Key STO functionalities.
+									</li>
+									<li>
+										Developed with VueJS, Golang, REST API,
+										JIRA, NEM Blockchain, MongoDB, Redis, and Agile.
+									</li>
+								</ul>
+							</div>
+							<div className="modal-bottom">
+								<a href="./todoList/index.html" target="_blank">
+									<h3>View live demo</h3>
+								</a>
+								<p
+									className="close-icon"
+									style={{
+										textAlign: "right",
+										fontWeight: 900,
+										fontSize: 2 + "rem",
+										cursor: "pointer"
+									}}
+									onClick={() => {
+										var modalBG = document.getElementById(
+											"gallery-card"
+										);
+										var todoModal = document.getElementById(
+											"todolist"
+										);
+										modalBG.style.display = "none";
+										todoModal.style.display = "none";
+									}}>
+									&#10005;
+								</p>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</React.Fragment>
 		);
