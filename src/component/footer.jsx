24b710f2c4cd09bad28788f72ad701ad2765bc0f
lonmars <marlonpamisa@gmail.com>
2020-12-01 17:32:34 +0800

    Changed footer links

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -12,20 +12,20 @@ class Footer extends React.Component {
 					<div className="row">
 						<div className="col-12 col-sm-12 col-md-8 mx-auto">
 							<a
-								href="https://codepen.io/kodi24fever"
+								href="https://codepen.io/lonmarsDev"
 								target="_blank"
 								rel="noopener noreferrer">
 								<img className="img-fluid" src={codepen} alt="" />
 							</a>
 							<a
 								className="px-3"
-								href="https://github.com/kodi24fever"
+								href="https://github.com/lonmarsDev"
 								target="_blank"
 								rel="noopener noreferrer">
 								<img src={github} alt="" />
 							</a>
 							<a
-								href="https://www.linkedin.com/in/frankizquierdo"
+								href="https://www.linkedin.com/in/marlon-pamisa-dev/"
 								target="_blank"
 								rel="noopener noreferrer">
 								<img src={linkedin} alt="" />
